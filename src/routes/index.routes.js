b8300ae766e6030f90455fd03c9721ef336ada8c
Cristian <cristianlarrauri1@gmail.com>
2023-05-11 14:03:34 -0300

    Fix index.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,13 @@
 const { Router } = require("express");
 
+const customers = require("./customers.routes");
+const barbers = require("./barbers.routes");
+const shifts = require("./shifts.routes");
+
 const router = Router();
+
+router.use("/customers", customers);
+router.use("/barbers", barbers);
+router.use("/shifts", shifts);
 
 module.exports = router;
