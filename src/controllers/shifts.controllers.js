a03e5bd49026e664911a67bcdbe4baeb2334528b
Cristian <cristianlarrauri1@gmail.com>
2023-05-11 10:41:04 -0300

    Add getShifts

---
+++ src/controllers/shifts.controllers.js
@@ -0,0 +1,75 @@
+const { Shifts } = require("../database");
+const moment = require("moment");
+
+const createShifts = async (req, res) => {
+  try {
+    // Obtener la fecha actual
+    const today = moment().startOf("day");
+
+    // Crear turnos para los próximos 14 días
+    for (let i = 0; i < 14; i++) {
+      const date = today.clone().add(i, "days");
+
+      // Generar turnos cada hora desde las 10:00 hasta las 20:00
+      for (let j = 10; j <= 20; j++) {
+        const time = moment({ hour: j }).format("HH:mm");
+
+        // Establecer el valor de "occupied" en verdadero si es domingo
+        const occupied = date.day() === 0;
+
+        // Crear el turno en la base de datos
+        await Shifts.create({ date, time, occupied });
+      }
+    }
+
+    return res.status(200).send("OK");
+  } catch (error) {
+    console.error("Error in createShifts", error);
+  }
+};
+
+//_____________________________________________________________
+
+const getShifts = async (req, res) => {
+  try {
+    let shifts = await Shifts.findAll();
+    return res.status(200).send(shifts);
+  } catch (error) {
+    console.error("Error in getShifts", error);
+  }
+};
+
+//_____________________________________________________________
+
+const getShiftsId = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error in getShiftsId", error);
+  }
+};
+
+//_____________________________________________________________
+
+const editShifts = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error in editShifts", error);
+  }
+};
+
+//_____________________________________________________________
+
+const deleteShifts = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error("Error in deleteShifts", error);
+  }
+};
+
+module.exports = {
+  createShifts,
+  getShifts,
+  getShiftsId,
+  editShifts,
+  deleteShifts,
+};
