ca3e0a34a18f05d96045b7bbddd64e747bc49791
Cristian <cristianlarrauri1@gmail.com>
2023-05-17 22:43:39 -0300

    Add barbersShifts.routes

---
+++ src/routes/barbersShifts.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const {
+  createBarbersShifts,
+  getBarbersShifts,
+  editBarbersShifts,
+  deleteBarbersShifts,
+  getBarbersShiftsId,
+} = require("../controllers/barbers.controllers");
+
+const router = Router();
+
+router.post("/", createBarbersShifts);
+router.get("/", getBarbersShifts);
+router.get("/:id", getBarbersShiftsId);
+router.put("/:id", editBarbersShifts);
+router.delete("/:id", deleteBarbersShifts);
+
+module.exports = router;
