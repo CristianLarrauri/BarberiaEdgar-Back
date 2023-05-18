3012c2a3b361d45c3bc7008dee9315e4f21a0acf
Cristian <cristianlarrauri1@gmail.com>
2023-05-17 22:43:54 -0300

    Add barbersShifts.controllers

---
+++ src/controllers/barbersShifts.controllers.js
@@ -0,0 +1,21 @@
+const {
+  Barbers,
+  Barbers_Shifts,
+  Shifts,
+  Customers,
+  Barbers_Shifts_Customers,
+} = require("../database");
+
+const createBarberShifts = () => {};
+const getBarberShifts = () => {};
+const getBarberShiftsId = () => {};
+const editBarberShifts = () => {};
+const deleteBarberShifts = () => {};
+
+module.exports = {
+  createBarbersShifts,
+  getBarbersShifts,
+  getBarbersShiftsId,
+  editBarbersShifts,
+  deleteBarbersShifts,
+};
