506a038446e6ec305d8174bfce0087963ad6b339
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-07-04 11:18:44 -0300

    Add incomes.routes and fix index.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const customers = require("./customers.routes");
 const barbers = require("./barbers.routes");
 const shifts = require("./shifts.routes");
+const incomes = require("./incomes.routes");
 
 const router = Router();
 
 router.use("/customers", customers);
 router.use("/barbers", barbers);
 router.use("/shifts", shifts);
+router.use("/incomes", incomes);
 
 module.exports = router;
