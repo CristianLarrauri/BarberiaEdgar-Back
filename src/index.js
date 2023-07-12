a74125dacd8cbd2de3642a139f63d0ee5499cd00
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-07-12 18:13:15 -0300

    Fix index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+const { default: axios } = require("axios");
 const server = require("./app.js");
-const { createBarbers } = require("./controllers/barbers.controllers.js");
 const { conn } = require("./database.js");
 require("dotenv").config();
 
@@ -7,9 +7,18 @@ const port = process.env.PORT || 3001;
 
 conn.sync({ force: true }).then(() => {
   server.listen(port, async () => {
-    await createBarbers({ body: { name: "Edgar" } });
-    await createBarbers({ body: { name: "Jere" } });
-    await createBarbers({ body: { name: "Adriano" } });
-    console.log(`Server listening on port: ${port}`);
+    try {
+      //______________________________________________________________
+      // Carga de datos cuando levanta el servidor
+      const barbersData = require("./barbers.json");
+      for (const barber of barbersData) {
+        await axios.post(`http://localhost:${port}/barbers`, barber);
+      }
+      //______________________________________________________________
+
+      console.log(`Server listening on port: ${port}`);
+    } catch (error) {
+      console.error(`Error on port: ${port}`, error);
+    }
   });
 });
