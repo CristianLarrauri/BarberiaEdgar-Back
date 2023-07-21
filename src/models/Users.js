e2597206665c7b7415deb938ab792d5269bda579
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-07-21 11:45:30 -0300

    Add model Users

---
+++ src/models/Users.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Users",
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        primaryKey: true,
+      },
+      ban: {
+        type: DataTypes.STRING,
+        defaultValue: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
